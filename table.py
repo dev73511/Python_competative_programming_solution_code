9a57582dd225a3f3a07fdf30ab6c42de29c1d1b1
Dev <dev73511@gmail.com>
2020-06-05 16:55:38 +0530

    There was a teacher in a small town who loves coding and he wants to create a program which prints out the whole multiplaction table of an entered numbered for his students can you help him to write a program in python?

---
+++ table.py
@@ -0,0 +1,8 @@
+# Question 1) There was a teacher in a small town who loves coding and he wants to create a program which prints out the whole multiplaction table of an entered numbered for his students can you help him to write a program in python?
+
+def table(tableof, upto):
+    for i in range(1, upto + 1):
+        print(tableof, "*", i, "=", (i*tableof))
+
+
+table(632, 10)
